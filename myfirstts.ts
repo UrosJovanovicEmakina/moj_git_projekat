16e814b3ac406825b78514068a48400d7079f474
Uros Jovanovic <uros_jovanovic@epam.com>
2024-02-12 20:20:04 +0100

    moj prvi komit

---
+++ myfirstts.ts
@@ -0,0 +1,32 @@
+class Animal {
+    name: string;
+    weight: number;
+
+    constructor(name: string, weight: number) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    animalInfoMain() {
+        console.log(`Animal: ${this.name} weights ${this.weight} kg and is `);
+    }
+}
+
+class Turtle extends Animal {
+    age: number;
+
+    constructor(name: string, weight: number, age: number) {
+        super(name, weight);
+        this.age = age;
+    }
+
+    animalInfo() {
+        this.animalInfoMain();
+        console.log(`${this.age} year/s old`);
+    }
+}
+
+const turtleMini = new Turtle("Mica", 1, 1);
+//const turtleCica = new Turtle("Cica", 2, 2, "Cici");
+turtleMini.animalInfo();
+//turtleCica.animalInfo();
