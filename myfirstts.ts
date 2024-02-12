1de571492b705729f1444c0dec0d99f3ef35bf7b
Uros Jovanovic <uros_jovanovic@epam.com>
2024-02-12 20:27:37 +0100

    moj drugi komit

--- myfirstts.ts
+++ myfirstts.ts
@@ -27,6 +27,4 @@ class Turtle extends Animal {
 }
 
 const turtleMini = new Turtle("Mica", 1, 1);
-//const turtleCica = new Turtle("Cica", 2, 2, "Cici");
 turtleMini.animalInfo();
-//turtleCica.animalInfo();
